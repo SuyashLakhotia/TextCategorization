9a9af95b0a76750b8bb7339a20b3ef30d068da92
Suyash Lakhotia <suyashlakhotia@gmail.com>
2017-12-26 01:50:27 +0700

    Added CNN Model

---
+++ text_cnn.py
@@ -0,0 +1,84 @@
+import numpy as np
+import tensorflow as tf
+
+
+class TextCNN(object):
+    """
+    A CNN architecture for text classification. Composed of an embedding layer, followed by parallel 
+    convolutional + max-pooling layer(s) and a softmax layer.
+    """
+
+    def __init__(self, sequence_length, num_classes, vocab_size, embedding_size, embeddings, filter_heights,
+                 num_features, l2_reg_lambda):
+        # Placeholders for input, output and dropout
+        self.input_x = tf.placeholder(tf.int32, [None, sequence_length], name="input_x")
+        self.input_y = tf.placeholder(tf.int32, [None], name="input_y")
+        self.dropout_keep_prob = tf.placeholder(tf.float32, name="dropout_keep_prob")
+
+        # Keeping track of L2 regularization loss
+        l2_loss = tf.constant(0.0)
+
+        # Embedding layer
+        with tf.name_scope("embedding"):
+            if embeddings is None:
+                self.embedding_mat = tf.Variable(tf.random_uniform([vocab_size, embedding_size], -1.0, 1.0),
+                                                 name="W")
+            else:
+                self.embedding_mat = tf.Variable(embeddings, trainable=True, name="W")
+            self.embedded_x = tf.nn.embedding_lookup(self.embedding_mat, self.input_x)
+            self.embedded_x = tf.expand_dims(self.embedded_x, -1)  # expand for .conv2d
+            self.embedded_x = tf.cast(self.embedded_x, tf.float32)
+
+        # Create a convolution + max-pool layer for each filter size (filter_height x embedding_size)
+        pooled_outputs = []
+        for i, filter_height in enumerate(filter_heights):
+            with tf.name_scope("conv-maxpool-{}".format(filter_height)):
+                # Convolution layer
+                filter_shape = [filter_height, embedding_size, 1, num_features]
+                W = tf.Variable(tf.truncated_normal(filter_shape, stddev=0.1), name="W")
+                b = tf.Variable(tf.constant(0.1, shape=[num_features]), name="b")
+                conv = tf.nn.conv2d(self.embedded_x,
+                                    W,
+                                    strides=[1, 1, 1, 1],
+                                    padding="VALID",
+                                    name="conv")
+                # Apply non-linearity
+                h = tf.nn.relu(tf.nn.bias_add(conv, b), name="relu")
+                # Max-pooling over the outputs
+                pooled = tf.nn.max_pool(value=h,
+                                        ksize=[1, sequence_length - filter_height + 1, 1, 1],
+                                        strides=[1, 1, 1, 1],
+                                        padding="VALID",
+                                        name="pool")
+                pooled_outputs.append(pooled)
+
+        # Combine all the pooled features
+        num_features_total = num_features * len(filter_heights)
+        self.h_pool = tf.concat(pooled_outputs, 3)
+        self.h_pool_flat = tf.reshape(self.h_pool, [-1, num_features_total])
+
+        # Add dropout
+        with tf.name_scope("dropout"):
+            self.h_drop = tf.nn.dropout(self.h_pool_flat, self.dropout_keep_prob)
+
+        # Final (unnormalized) scores and predictions
+        with tf.name_scope("output"):
+            W = tf.get_variable("W",
+                                shape=[num_features_total, num_classes],
+                                initializer=tf.contrib.layers.xavier_initializer())
+            b = tf.Variable(tf.constant(0.1, shape=[num_classes]), name="b")
+            l2_loss += tf.nn.l2_loss(W)
+            l2_loss += tf.nn.l2_loss(b)
+            self.scores = tf.nn.xw_plus_b(self.h_drop, W, b, name="scores")
+            self.predictions = tf.argmax(self.scores, 1, name="predictions")
+            self.predictions = tf.cast(self.predictions, tf.int32)
+
+        # Calculate mean cross-entropy loss
+        with tf.name_scope("loss"):
+            losses = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=self.scores, labels=self.input_y)
+            self.loss = tf.reduce_mean(losses) + l2_reg_lambda * l2_loss
+
+        # Calculate accuracy
+        with tf.name_scope("accuracy"):
+            correct_predictions = tf.equal(self.predictions, self.input_y)
+            self.accuracy = tf.reduce_mean(tf.cast(correct_predictions, "float"), name="accuracy")
---
+++ v1_train.py
@@ -0,0 +1,199 @@
+import os
+import time
+import datetime
+
+import numpy as np
+import tensorflow as tf
+from tensorflow.contrib import learn
+
+import data
+from text_cnn import TextCNN
+
+
+# Parameters
+# ==================================================
+
+# Pre-trained word embeddings
+embedding_dim = 300  # dimensionality of embedding
+embedding_file = "data/GoogleNews-vectors-negative300.bin"  # word embeddings file
+
+# Model parameters
+filter_heights = "3,4,5"  # comma-separated filter heights
+num_features = 128  # number of features per filter
+
+# Training parameters
+learning_rate = 1e-3
+batch_size = 64
+num_epochs = 200
+
+# Regularization parameters
+dropout_keep_prob = 0.5  # dropout keep probability
+l2_reg_lambda = 0.0  # L2 regularization lambda
+
+# Misc. parameters
+evaluate_every = 100  # evaluate model on test set after this many steps
+checkpoint_every = 100  # save model after this many steps
+num_checkpoints = 5  # number of checkpoints to store
+
+allow_soft_placement = True  # allow device soft device placement i.e. fall back on available device
+log_device_placement = False  # log placement of operations on devices
+
+
+# Data Preparation
+# ==================================================
+
+print("Loading training data...")
+train = data.Text20News(subset="train")  # load data
+train.remove_short_documents(nwords=20, vocab='full')  # remove documents < 20 words in length
+train.clean_text()  # tokenize & clean text
+train.vectorize(stop_words="english")  # create term-document matrix and vocabulary
+train.remove_encoded_images()  # remove encoded images
+
+print("Loading test data...")
+test = data.Text20News(subset="test")
+test.clean_text()  # tokenize & clean text
+test.vectorize(vocabulary=train.vocab)  # create term-document matrix using train.vocab
+test.remove_encoded_images()  # remove encoded images
+
+x_train = train.documents
+x_test = test.documents
+y_train = train.labels
+y_test = test.labels
+
+# Transform documents to arrays of vocabulary indices
+print("Transforming documents...")
+max_document_length = max([len(x.split(" ")) for x in x_train] + [len(x.split(" ")) for x in x_test])
+vocab_processor = learn.preprocessing.VocabularyProcessor(max_document_length)
+x_train = np.array(list(vocab_processor.fit_transform(x_train)))
+x_test = np.array(list(vocab_processor.transform(x_test)))
+
+print("Vocabulary Size: {}".format(len(vocab_processor.vocabulary_)))
+print("Max. Document Length: {}".format(max_document_length))
+print("Number of Classes: {}".format(len(train.class_names)))
+print("Train/Test Split: {}/{}".format(len(y_train), len(y_test)))
+
+# Process Google News word2vec file (in a memory-friendly way) and store relevant embeddings.
+print("Loading pre-trained embeddings from {}...".format(embedding_file))
+embeddings = data.load_word2vec(embedding_file, vocab_processor.vocabulary_, embedding_dim, tf_VP=True)
+
+
+# Training
+# ==================================================
+
+with tf.Graph().as_default():
+    session_conf = tf.ConfigProto(allow_soft_placement=allow_soft_placement,
+                                  log_device_placement=log_device_placement)
+    sess = tf.Session(config=session_conf)
+    with sess.as_default():
+        cnn = TextCNN(sequence_length=x_train.shape[1],
+                      num_classes=y_train.shape[1],
+                      vocab_size=len(vocab_processor.vocabulary_),
+                      embedding_size=embedding_dim,
+                      embeddings=embeddings,
+                      filter_heights=list(map(int, filter_heights.split(","))),
+                      num_features=num_features,
+                      l2_reg_lambda=l2_reg_lambda)
+
+        # Define training procedure
+        global_step = tf.Variable(0, name="global_step", trainable=False)
+        optimizer = tf.train.AdamOptimizer(learning_rate)
+        grads_and_vars = optimizer.compute_gradients(cnn.loss)
+        train_op = optimizer.apply_gradients(grads_and_vars, global_step=global_step)
+
+        # Keep track of gradient values and sparsity
+        grad_summaries = []
+        for g, v in grads_and_vars:
+            if g is not None:
+                grad_hist_summary = tf.summary.histogram("{}/grad/hist".format(v.name), g)
+                sparsity_summary = tf.summary.scalar("{}/grad/sparsity".format(v.name), tf.nn.zero_fraction(g))
+                grad_summaries.append(grad_hist_summary)
+                grad_summaries.append(sparsity_summary)
+        grad_summaries_merged = tf.summary.merge(grad_summaries)
+
+        # Output directory for models and summaries
+        timestamp = str(int(time.time()))
+        out_dir = os.path.abspath(os.path.join(os.path.curdir, "runs", "v1", timestamp))
+        print("Writing to {}\n".format(out_dir))
+
+        # Summaries for loss and accuracy
+        loss_summary = tf.summary.scalar("loss", cnn.loss)
+        acc_summary = tf.summary.scalar("accuracy", cnn.accuracy)
+
+        # Train Summaries
+        train_summary_op = tf.summary.merge([loss_summary, acc_summary, grad_summaries_merged])
+        train_summary_dir = os.path.join(out_dir, "summaries", "train")
+        train_summary_writer = tf.summary.FileWriter(train_summary_dir, sess.graph)
+
+        # Test Summaries
+        test_summary_op = tf.summary.merge([loss_summary, acc_summary])
+        test_summary_dir = os.path.join(out_dir, "summaries", "test")
+        test_summary_writer = tf.summary.FileWriter(test_summary_dir, sess.graph)
+
+        # Checkpoint directory & saver
+        checkpoint_dir = os.path.abspath(os.path.join(out_dir, "checkpoints"))
+        checkpoint_prefix = os.path.join(checkpoint_dir, "model")
+        if not os.path.exists(checkpoint_dir):
+            os.makedirs(checkpoint_dir)
+        saver = tf.train.Saver(tf.global_variables(), max_to_keep=num_checkpoints)
+
+        # Write vocabulary
+        vocab_processor.save(os.path.join(out_dir, "vocab"))
+
+        # Initialize all variables
+        sess.run(tf.global_variables_initializer())
+
+        def train_step(x_batch, y_batch):
+            """
+            A single training step.
+            """
+            feed_dict = {
+                cnn.input_x: x_batch,
+                cnn.input_y: y_batch,
+                cnn.dropout_keep_prob: dropout_keep_prob
+            }
+            _, step, summaries, loss, accuracy = sess.run([train_op, global_step, train_summary_op, cnn.loss,
+                                                           cnn.accuracy],
+                                                          feed_dict)
+            time_str = datetime.datetime.now().isoformat()
+            print("{}: Step {}, Loss {:g}, Accuracy {:g}".format(time_str, step, loss, accuracy))
+            train_summary_writer.add_summary(summaries, step)
+
+        def test_step(x_batch, y_batch, writer=None):
+            """
+            Evaluates model on a test set.
+            """
+            feed_dict = {
+                cnn.input_x: x_batch,
+                cnn.input_y: y_batch,
+                cnn.dropout_keep_prob: 1.0
+            }
+            step, summaries, loss, accuracy = sess.run([global_step, test_summary_op, cnn.loss,
+                                                        cnn.accuracy],
+                                                       feed_dict)
+            time_str = datetime.datetime.now().isoformat()
+            print("{}: Step {}, Loss {:g}, Accuracy {:g}".format(time_str, step, loss, accuracy))
+            if writer:
+                writer.add_summary(summaries, step)
+            return accuracy
+
+        # Generate batches
+        batches = data.batch_iter(list(zip(x_train, y_train)), batch_size, num_epochs)
+
+        # Maximum test accuracy
+        max_accuracy = 0
+
+        # Training loop
+        for batch in batches:
+            x_batch, y_batch = zip(*batch)
+            train_step(x_batch, y_batch)
+            current_step = tf.train.global_step(sess, global_step)
+            if current_step % evaluate_every == 0:
+                print("\nEvaluation:")
+                accuracy = test_step(x_test, y_test, writer=test_summary_writer)
+                if accuracy > max_accuracy:
+                    max_accuracy = accuracy
+                print("Max. Test Accuracy: {:g}".format(max_accuracy))
+                print("")
+            if current_step % checkpoint_every == 0:
+                path = saver.save(sess, checkpoint_prefix, global_step=current_step)
+                print("Saved model checkpoint to {}\n".format(path))
