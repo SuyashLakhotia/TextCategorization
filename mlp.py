3f55b771fe95a95dc38c84bad10948fedb89bdc0
Suyash Lakhotia <suyashlakhotia@gmail.com>
2017-12-29 22:06:56 +0700

    Implemented MLP

---
+++ mlp.py
@@ -0,0 +1,61 @@
+import tensorflow as tf
+
+
+class MLP(object):
+    """
+    A multi-layer perceptron with flexible number of fully-connected layers followed by a softmax layer.
+    """
+
+    def __init__(self, vocab_size, num_classes, layers, l2_reg_lambda):
+        # Placeholders for input, output and dropout
+        self.input_x = tf.placeholder(tf.float32, [None, vocab_size], name="input_x")
+        self.input_y = tf.placeholder(tf.int32, [None], name="input_y")
+        self.dropout_keep_prob = tf.placeholder(tf.float32, name="dropout_keep_prob")
+
+        # Keeping track of L2 regularization loss
+        l2_loss = tf.constant(0.0)
+
+        # Create fully-connected layers
+        x = self.input_x
+        for i, num_units in enumerate(layers):
+            with tf.variable_scope("fc-{}".format(num_units)):
+                W = tf.get_variable("W",
+                                    shape=[x.get_shape().as_list()[1], num_units],
+                                    initializer=tf.contrib.layers.xavier_initializer())
+                b = tf.Variable(tf.constant(0.1, shape=[num_units]), name="b")
+
+                l2_loss += tf.nn.l2_loss(W)
+                l2_loss += tf.nn.l2_loss(b)
+
+                x = tf.nn.xw_plus_b(x, W, b)
+                x = tf.nn.relu(x)
+                x = tf.nn.dropout(x, self.dropout_keep_prob)
+
+        # Final (unnormalized) scores and predictions
+        with tf.variable_scope("output"):
+            if len(layers) == 0:
+                num_in = vocab_size
+            else:
+                num_in = layers[-1]
+
+            W = tf.get_variable("W",
+                                shape=[num_in, num_classes],
+                                initializer=tf.contrib.layers.xavier_initializer())
+            b = tf.Variable(tf.constant(0.1, shape=[num_classes]), name="b")
+
+            l2_loss += tf.nn.l2_loss(W)
+            l2_loss += tf.nn.l2_loss(b)
+
+            self.scores = tf.nn.xw_plus_b(x, W, b, name="scores")
+            self.predictions = tf.argmax(self.scores, 1, name="predictions")
+            self.predictions = tf.cast(self.predictions, tf.int32)
+
+        # Calculate mean cross-entropy loss
+        with tf.variable_scope("loss"):
+            losses = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=self.scores, labels=self.input_y)
+            self.loss = tf.reduce_mean(losses) + l2_reg_lambda * l2_loss
+
+        # Calculate accuracy
+        with tf.variable_scope("accuracy"):
+            correct_predictions = tf.equal(self.predictions, self.input_y)
+            self.accuracy = tf.reduce_mean(tf.cast(correct_predictions, "float"), name="accuracy")
---
+++ mlp_train.py
@@ -0,0 +1,94 @@
+import os
+import time
+import subprocess
+
+import numpy as np
+import tensorflow as tf
+
+import data
+from mlp import MLP
+from train import train_and_test
+
+
+model_name = "mlp"
+
+
+# Parameters
+# ==================================================
+
+# Model parameters
+fc_layers = []  # number of units in fully-connected layers
+
+# Training parameters
+learning_rate = 1e-3
+batch_size = 64
+num_epochs = 200
+
+# Regularization parameters
+dropout_keep_prob = 0.5  # dropout keep probability
+l2_reg_lambda = 0.0  # L2 regularization lambda
+
+# Misc. parameters
+allow_soft_placement = True  # allow device soft device placement i.e. fall back on available device
+log_device_placement = False  # log placement of operations on devices
+
+
+# Data Preparation
+# ==================================================
+
+print("Loading training data...")
+train = data.Text20News(subset="train")
+train.preprocess_train(out="tfidf", norm="l1")
+
+print("Loading test data...")
+test = data.Text20News(subset="test")
+test.preprocess_test(train_vocab=train.vocab, out="tfidf", norm="l1")
+
+x_train = train.data_tfidf.astype(np.float32)
+x_test = test.data_tfidf.astype(np.float32)
+y_train = train.labels
+y_test = test.labels
+
+print("")
+print("Vocabulary Size: {}".format(train.orig_vocab_size))
+print("Vocabulary Size (Reduced): {}".format(len(train.vocab)))
+print("Number of Classes: {}".format(len(train.class_names)))
+print("Train/Test Split: {}/{}".format(len(y_train), len(y_test)))
+print("")
+print("x_train: {}".format(x_train.shape))
+print("x_test: {}".format(x_test.shape))
+print("y_train: {}".format(y_train.shape))
+print("y_test: {}".format(y_test.shape))
+print("")
+
+
+# Training
+# ==================================================
+
+with tf.Graph().as_default():
+    session_conf = tf.ConfigProto(allow_soft_placement=allow_soft_placement,
+                                  log_device_placement=log_device_placement)
+    sess = tf.Session(config=session_conf)
+    with sess.as_default():
+        mlp = MLP(vocab_size=len(train.vocab),
+                  num_classes=len(train.class_names),
+                  layers=fc_layers,
+                  l2_reg_lambda=l2_reg_lambda)
+
+        # Output directory for models and summaries
+        timestamp = str(int(time.time()))
+        out_dir = os.path.abspath(os.path.join(os.path.curdir, "runs", model_name, timestamp))
+
+        # Convert sparse matrices to arrays
+        x_train = x_train.toarray()
+        x_test = x_test.toarray()
+
+        # Train and test model
+        max_accuracy = train_and_test(sess, mlp, x_train, y_train, x_test, y_test, learning_rate, batch_size,
+                                      num_epochs, dropout_keep_prob, out_dir)
+
+        # Output for results.csv
+        hyperparams = "{{layers: {}}".format(fc_layers)
+        latest_git = subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD']).decode().strip()
+        print("\"{}\",\"{}\",\"{:.9f}\",\"{}\",\"{}\"".format(model_name, hyperparams,
+                                                              max_accuracy, latest_git, timestamp))
