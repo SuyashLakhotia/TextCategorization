789e4054d39d4b5ad57455c6cbf3a8371ed09fc4
Suyash Lakhotia <suyashlakhotia@gmail.com>
2018-02-26 22:54:28 +0800

    Added Args for Baseline Models

--- baseline.py
+++ baseline.py
@@ -23,6 +23,14 @@ parser.add_argument("--vocab_size", type=int, default=None,
 parser.add_argument("--out", type=str, default="tfidf", choices=["tfidf", "count"],
                     help="Type of document vectors (default: tfidf)")
 
+parser.add_argument("--model", type=str, default="all", choices=["all", "linear_svc", "multinomial_nb"],
+                    help="Model(s) to run on dataset.")
+
+parser.add_argument("--LSVC-C", type=float, default=1.0, dest="C",
+                    help="LinearSVC: Penalty parameter C of the error term.")
+parser.add_argument("--MNB-a", type=float, default=0.01, dest="alpha",
+                    help="MultinomialNB: Additive smoothing parameter (0 for no smoothing).")
+
 parser.add_argument("--no-save", action="store_false", dest="save",
                     help="Include this flag if models should not be pickled.")
 parser.set_defaults(save=True)
@@ -50,32 +58,36 @@ data_str = "{{format: '{}', vocab_size: {}}}".format(args.out, len(train.vocab))
 # Training
 # ==================================================
 
+timestamp = str(int(time.time()))
+
 # Linear Support Vector Classifier
-svm_clf = LinearSVC()
-svm_clf.fit(x_train, y_train)
-predicted = svm_clf.predict(x_test)
-svm_acc = np.mean(predicted == y_test)
+if args.model == "all" or args.model == "linear_svc":
+    svm_clf = LinearSVC(C=args.C)
+    svm_clf.fit(x_train, y_train)
+    predicted = svm_clf.predict(x_test)
+    svm_acc = np.mean(predicted == y_test)
+    utils.print_result(args.dataset, "linear_svc", svm_acc, data_str, timestamp)
 
 # Multinomial Naive Bayes Classifier
-bayes_clf = MultinomialNB(alpha=0.01)
-bayes_clf.fit(x_train, y_train)
-predicted = bayes_clf.predict(x_test)
-bayes_acc = np.mean(predicted == y_test)
-
-# Output for results.csv
-timestamp = str(int(time.time()))
-utils.print_result(args.dataset, "linear_svc", svm_acc, data_str, timestamp)
-utils.print_result(args.dataset, "multinomial_nb", bayes_acc, data_str, timestamp)
+if args.model == "all" or args.model == "multinomial_nb":
+    bayes_clf = MultinomialNB(alpha=args.alpha)
+    bayes_clf.fit(x_train, y_train)
+    predicted = bayes_clf.predict(x_test)
+    bayes_acc = np.mean(predicted == y_test)
+    utils.print_result(args.dataset, "multinomial_nb", bayes_acc, data_str, timestamp)
 
 # Save models as pickles
 if args.save:
-    out_dir = os.path.abspath(os.path.join(os.path.curdir, "runs", args.dataset, "linear_svc", timestamp))
-    if not os.path.exists(out_dir):
-        os.makedirs(out_dir)
-    pickle.dump(svm_clf, open(out_dir + "/pickle.pkl", "wb"))
+    if args.model == "all" or args.model == "linear_svc":
+        out_dir = os.path.abspath(os.path.join(os.path.curdir, "runs", args.dataset, "linear_svc",
+                                               timestamp))
+        if not os.path.exists(out_dir):
+            os.makedirs(out_dir)
+        pickle.dump(svm_clf, open(out_dir + "/pickle.pkl", "wb"))
 
-    out_dir = os.path.abspath(os.path.join(os.path.curdir, "runs", args.dataset, "multinomial_nb",
-                                           timestamp))
-    if not os.path.exists(out_dir):
-        os.makedirs(out_dir)
-    pickle.dump(bayes_clf, open(out_dir + "/pickle.pkl", "wb"))
+    if args.model == "all" or args.model == "multinomial_nb":
+        out_dir = os.path.abspath(os.path.join(os.path.curdir, "runs", args.dataset, "multinomial_nb",
+                                               timestamp))
+        if not os.path.exists(out_dir):
+            os.makedirs(out_dir)
+        pickle.dump(bayes_clf, open(out_dir + "/pickle.pkl", "wb"))
